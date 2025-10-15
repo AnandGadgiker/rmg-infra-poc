57b6b9f4fe167433ef7d15e0eee809f51dcc44b6
Akashdpakash <54543615+Akashdpakash@users.noreply.github.com>
2025-10-16 01:02:03 +0530

    updating vars

--- IAC/modules/outputs/main.tf
+++ IAC/modules/outputs/main.tf
@@ -1,35 +1,3 @@
-variable "keyvault_id" {
-  type = string
-}
-
-variable "cosmosdb_name" {
-  type = string
-}
-
-variable "acr_name" {
-  type = string
-}
-
-variable "storage_account_name" {
-  type = string
-}
-
-variable "eventhub_namespace" {
-  type = string
-}
-
-variable "eventhub_name" {
-  type = string
-}
-
-variable "app_service_name" {
-  type = string
-}
-
-variable "app_service_default_hostname" {
-  type = string
-}
-
 output "keyvault_id" {
   value = var.keyvault_id
 }
--- IAC/modules/outputs/variables.tf
+++ IAC/modules/outputs/variables.tf
@@ -1,8 +1,31 @@
-variable "keyvault_id" { type = string }
-variable "cosmosdb_name" { type = string }
-variable "acr_name" { type = string }
-variable "storage_account_name" { type = string }
-variable "eventhub_namespace" { type = string }
-variable "eventhub_name" { type = string }
-variable "app_service_name" { type = string }
-variable "app_service_default_hostname" { type = string }
+variable "keyvault_id" {
+  type = string
+}
+
+variable "cosmosdb_name" {
+  type = string
+}
+
+variable "acr_name" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "eventhub_namespace" {
+  type = string
+}
+
+variable "eventhub_name" {
+  type = string
+}
+
+variable "app_service_name" {
+  type = string
+}
+
+variable "app_service_default_hostname" {
+  type = string
+}
