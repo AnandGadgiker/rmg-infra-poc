924dc761507408d949082da47c1c065a6e0179eb
Akashdpakash <54543615+Akashdpakash@users.noreply.github.com>
2025-10-16 00:07:32 +0530

    updating naming convention

--- IAC/environments/uat/main.tf
+++ IAC/environments/uat/main.tf
@@ -1,50 +1,54 @@
-module "keyvault" {
-  source              = "../../modules/keyvault"
-  key_vault_name      = var.key_vault_name
-  location            = var.location
+# Key Vault
+module "kv" {
+  source        = "../../modules/keyvault"
+  key_vault_name = var.key_vault_name
+  location       = var.location
   resource_group_name = var.resource_group_name
-  tenant_id           = var.tenant_id
-  aad_client_secret_value = var.aad_client_secret_value
+  tenant_id      = var.tenant_id
+  object_id      = var.object_id
 }
 
-module "app_service" {
+# App Service Plan + App Service
+module "app_svc" {
   source                = "../../modules/app_service"
   app_service_plan_name = var.app_service_plan_name
   app_service_name      = var.app_service_name
   location              = var.location
   resource_group_name   = var.resource_group_name
-  app_settings          = {
-    ENV               = var.env
-    AAD_CLIENT_SECRET = module.keyvault.aad_client_secret_name
-  }
+  app_settings          = var.app_settings
   subnet_id             = var.subnet_id
+  app_service_plan_id   = var.app_service_plan_id
 }
 
-module "cosmosdb" {
+# Cosmos DB
+module "cosmos" {
   source              = "../../modules/cosmosdb"
   cosmosdb_name       = var.cosmosdb_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  key_vault_key_id    = module.keyvault.key_vault_key_id
+  key_vault_key_id    = module.kv.key_vault_key_id
 }
 
+# Azure Container Registry
 module "acr" {
   source              = "../../modules/acr"
   acr_name            = var.acr_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  key_vault_key_id    = module.keyvault.key_vault_key_id
+  key_vault_key_id    = module.kv.key_vault_key_id
 }
 
-module "storage_account" {
+# Storage Account
+module "stg" {
   source               = "../../modules/storage_account"
   storage_account_name = var.storage_account_name
   location             = var.location
   resource_group_name  = var.resource_group_name
-  key_vault_key_id     = module.keyvault.key_vault_key_id
+  key_vault_key_id     = module.kv.key_vault_key_id
 }
 
-module "eventhub" {
+# Event Hub
+module "eh" {
   source              = "../../modules/eventhub"
   eventhub_namespace  = var.eventhub_namespace
   eventhub_name       = var.eventhub_name
@@ -53,13 +57,14 @@ module "eventhub" {
   subnet_id           = var.subnet_id
 }
 
+# Outputs
 module "uat_outputs" {
   source                = "../../modules/outputs"
-  keyvault_id           = module.keyvault.key_vault_id
-  cosmosdb_name         = module.cosmosdb.cosmosdb_name
+  keyvault_id           = module.kv.key_vault_id
+  cosmosdb_name         = module.cosmos.cosmosdb_name
   acr_name              = module.acr.acr_name
-  storage_account_name  = module.storage_account.storage_account_name
-  eventhub_namespace    = module.eventhub.eventhub_namespace
-  eventhub_name         = module.eventhub.eventhub_name
+  storage_account_name  = module.stg.storage_account_name
+  eventhub_namespace    = module.eh.eventhub_namespace
+  eventhub_name         = module.eh.eventhub_name
   app_service_name      = module.app_service.app_service_name
 }
