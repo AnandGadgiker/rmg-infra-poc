456fb2262a4081e2f0c19b27ffe0dab2661026cd
akashdheev <akash.dheev@hcltech.com>
2025-10-15 14:10:04 +0000

    updating var

--- IAC/modules/cosmosdb/variables.tf
+++ IAC/modules/cosmosdb/variables.tf
@@ -1,3 +1,7 @@
 variable "cosmosdb_name" {}
 variable "location" {}
 variable "resource_group_name" {}
+variable "key_vault_key_id" {
+  type        = string
+  description = "Key Vault Key ID for customer-managed key encryption"
+}
