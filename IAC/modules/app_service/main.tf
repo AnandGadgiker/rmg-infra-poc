37b8676ddbe3bfe49e51b392f12c7942e9f3cfaf
akashdheev <akash.dheev@hcltech.com>
2025-10-15 10:07:01 +0000

    updating modules

--- IAC/modules/app_service/main.tf
+++ IAC/modules/app_service/main.tf
@@ -1,3 +1,12 @@
+variable "app_service_plan_name" {}
+variable "app_service_name" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "app_settings" {
+  type = map(string)
+}
+variable "subnet_id" {}
+
 resource "azurerm_app_service_plan" "plan" {
   name                = var.app_service_plan_name
   location            = var.location
@@ -17,7 +26,13 @@ resource "azurerm_app_service" "app" {
 
   site_config {
     scm_type = "LocalGit"
+    vnet_route_all_enabled = true
   }
 
   app_settings = var.app_settings
+}
+
+resource "azurerm_app_service_virtual_network_swift_connection" "vnet_integration" {
+  app_service_id = azurerm_app_service.app.id
+  subnet_id      = var.subnet_id
 }
