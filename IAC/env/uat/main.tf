fc02fa843b51a5570c8b2fbe74e231d25d33cd5e
Akashdpakash <54543615+Akashdpakash@users.noreply.github.com>
2025-10-16 04:57:06 +0530

    updating commands

--- IAC/env/uat/main.tf
+++ IAC/env/uat/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
 # Key Vault
 module "kv" {
   source                  = "../../modules/keyvault"
