fa3172bd56f3d7a730f93a556c1ea65a4501cabf
Akashdpakash <54543615+Akashdpakash@users.noreply.github.com>
2025-10-16 21:00:23 +0530

    updating vars

--- IAC/env/uat/variables.tf
+++ IAC/env/uat/variables.tf
@@ -50,3 +50,14 @@ variable "key_vault_name" {
   type = string
 }
 
+variable "terraform_sp_object_id" {
+  type        = string
+  description = "Object ID of the SP or Managed Identity running Terraform"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Tags to assign resources"
+}
+
