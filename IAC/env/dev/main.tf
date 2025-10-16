07b3c1b29b1d1bde0e21b5fd50472c098c309314
akashdheev <akash.dheev@hcltech.com>
2025-10-16 13:46:12 +0000

    updating providers

--- IAC/env/dev/main.tf
+++ IAC/env/dev/main.tf
@@ -10,12 +10,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-
 }
 
 # 1️⃣ Create Resource Group first
