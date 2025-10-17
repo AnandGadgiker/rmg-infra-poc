9755591c1f8a80eab21afff70cb80db646ac18fe
Akashdpakash <54543615+Akashdpakash@users.noreply.github.com>
2025-10-17 07:55:39 +0530

    updating vars

--- IAC/modules/storage_account/main.tf
+++ IAC/modules/storage_account/main.tf
@@ -1,3 +1,4 @@
+# Storage Account
 resource "azurerm_storage_account" "storage" {
   name                          = var.storage_account_name
   resource_group_name           = var.resource_group_name
@@ -8,7 +9,6 @@ resource "azurerm_storage_account" "storage" {
   is_hns_enabled                = true
   public_network_access_enabled = false
 
-  # Merge mandatory policy tags with user-provided tags
   tags = merge(
     var.tags,
     {
@@ -38,33 +38,9 @@ resource "azurerm_storage_account" "storage" {
     expiration_period = "PT24H"
   }
 
-  # Placeholder for CMK (required but ignored)
-  customer_managed_key {}
-
-  lifecycle {
-    ignore_changes = [
-      customer_managed_key,
-    ]
-  }
-}
-
-# Customer Managed Key linking Storage Account to Key Vault
-resource "azurerm_storage_account_customer_managed_key" "stg_cmk" {
-  storage_account_id = azurerm_storage_account.storage.id
-  key_vault_key_id   = var.key_vault_key_id
-}
-
-# Private Endpoint
-resource "azurerm_private_endpoint" "storage_pe" {
-  name                = "storage-private-endpoint"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = var.subnet_id
-
-  private_service_connection {
-    name                           = "storage-priv-conn"
-    private_connection_resource_id = azurerm_storage_account.storage.id
-    subresource_names              = ["blob"]
-    is_manual_connection           = false
+  # Enable CMK
+  customer_managed_key {
+    key_vault_key_id          = var.key_vault_key_id          # pass the Key Vault key
+    user_assigned_identity_id = var.user_assigned_identity_id # must be a UAMI
   }
 }
